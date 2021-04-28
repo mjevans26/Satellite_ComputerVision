961bd349469103019d12b74790333d85884be921
mjevans26 <evans.michael26@gmail.com>
2021-04-28 15:58:31 -0400

    update make_confusion_matrix to reference con_mat

--- utils/model_tools.py
+++ utils/model_tools.py
@@ -167,8 +167,9 @@ def make_confusion_matrix(dataset, model, multiclass = False):
       # create confusion matrix containing raw counts from current data
       con_mat_current = tf.math.confusion_matrix(labels = labels, predictions = preds, num_classes = 2).numpy()
       # get row sums
-      rowsums_current = con_mat_current.sum(axis = 1)
+#      rowsums_current = con_mat_current.sum(axis = 1)
       # if we're on the first batch
+      con_mat = []
       if i == 0:
         con_mat = con_mat_current
       else:
