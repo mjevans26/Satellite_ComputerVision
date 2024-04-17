ca3cf638cfb1447b33227a479428e30deb9f0528
Mike Evans <mevans@chesapeakeconservancy.org>
2024-04-17 10:17:07 -0400

    add crs argument to get_ssurgo_stac

--- utils/pc_tools.py
+++ utils/pc_tools.py
@@ -229,19 +229,15 @@ def get_s2_stac(dates, aoi, cloud_thresh = 10):
     clipped = s2projected.rio.clip(geometries = [aoi], crs = 4326)
     return clipped
 
-def get_ssurgo_stac(aoi)-> np.ndarray:
+def get_ssurgo_stac(aoi, epsg)-> np.ndarray:
     """Sample ssurgo data in raster format
     
     Parameters
     ---
     catalog: pystac_client.client.Client
         planetary computer catalog
-    aoi: shapely.geometry.Polygon
-        geometry to filter ssurgo data via intersection
-    point: geopandas.geoseries.GeoSeries
-        point at which to sample data
-    window_size: int
-        size in METERS of each side of the chip to be sampled
+    epsg: int
+        cooridnate reference system epsg code to reproject ssurgo data to
     
     Returns
     ---
@@ -260,7 +256,10 @@ def get_ssurgo_stac(aoi)-> np.ndarray:
     surgo = surgoitems[0]
 
     surgowkt = surgo['properties']['proj:wkt2']
-    surgoCrs = CRS.from_wkt(surgowkt)
+    if epsg:
+        surgoCrs = epsg
+    else:
+        surgoCrs = CRS.from_wkt(surgowkt)
 
     # surgoepsg = surgo['properties']['proj:epsg']
     surgoStac = stackstac.stack(
