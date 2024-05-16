897b9ff5f6e6b6d2e6e6a4054db63cc66e44cfb1
Mike Evans <mevans@chesapeakeconservancy.org>
2024-05-16 11:22:14 -0400

    update stats.py

--- utils/stats.py
+++ utils/stats.py
@@ -2,14 +2,42 @@ import numpy as np
 from scipy.special import gamma
 
 def gamma_pdf(x, a, b):
-    """calculate the pdf of a gamma distribution defined by shape a and scale b"""
+    """calculate the pdf of a gamma distribution defined by shape a and scale b
+    Params
+    ---
+    x: float or array
+      values at which to evaluate the gamma pdf
+    a: float or array
+      shape parameter of the gamma distribution
+    b: float or array
+      scale parameter of the gamma distribution
+    
+    Return
+    ---
+    float or array:
+      probability of x under the gamma distribution with shape a and scale b
+    """
     denom = gamma(a)*(b**a)
     num = (x**(a-1))*(np.exp(-1*x/b))
     pd = num/denom
     return pd
 
 def lognormal_pdf(x, u, v):
-    """calculate the pdf of a lognormal distribution defined by mean u and variance v"""
+    """calculate the pdf of a lognormal distribution defined by mean u and variance v
+    Params
+    ---
+    x: float or array
+      values at which to evaluate the lognormal pdf
+    u: float or array
+      mean of the lognormal distribution
+    v: float or array
+      variance of the lognormal distribution
+    
+    Return
+    ---
+    float or array:
+      probability of x under the lognormal distribution with mean u and variance v
+    """
     sd = np.sqrt(v)
     const = (pi*2)**0.5
     first = 1/(sd*const)
